435a08720f98b77ab4a9f23cd38b094a06cd930d
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-02-21 11:56:39 +0530

    chore: added not found page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import { AuthProvider } from "./context/AuthContext";
@@ -9,6 +9,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import ProtectedLayout from "./components/ProtectedLayout";
 import UploadImg from "./pages/UploadImg";
 import CreateAlbum from "./pages/CreateAlbum";
+import PageNoteFound from "./pages/PageNoteFound";
 
 function App() {
   return (
@@ -18,11 +19,13 @@ function App() {
           <Routes>
             <Route path="/" element={<SignUp />}></Route>
             <Route path="/SignIn" element={<SignIn />}></Route>
+            {/* <Route path="/" element={<Navigate to="/SignIn"/>}/> */}
             <Route element={<ProtectedRoute />}>
               <Route element={<ProtectedLayout />}>
                 <Route path="/galleryPick" element={<GalleryPick />}></Route>
                 <Route path="/uploadImg" element={<UploadImg />}></Route>
                 <Route path="/createAlbum" element={<CreateAlbum />}></Route>
+                <Route path="*" element={<PageNoteFound />}></Route>
               </Route>
             </Route>
           </Routes>
---
+++ src/pages/PageNoteFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNoteFound() {
+  return (
+    <div> 404 Page Note Found</div>
+  )
+}
+
+export default PageNoteFound
