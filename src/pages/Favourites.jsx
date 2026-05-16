70f4c0a8443c5ba5ad14733a3087cad433cc2059
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-05-16 23:35:39 +0530

    feat: modified files

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./favourites.css";
+import './galleryPick.css'; 
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap-icons/font/bootstrap-icons.css";
