4833a9e5eaf3e64070c79246f665727e40938725
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-05-16 20:47:26 +0530

    feat: modified files

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./favourites.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -122,17 +123,8 @@ const Favourites = () => {
 
       <div className="favImgContainer">
         {favImages.map((img) => (
-          <div
-            key={img._id}
-            className="imgContainer favImgHeight"
-            
-          >
-            <img
-              src={img.imgUrl}
-              alt="image"
-              className="favImgStyl"
-              
-            />
+          <div key={img._id} className="imgContainer favImgHeight">
+            <img src={img.imgUrl} alt="image" className="favImgStyl" />
             <button
               className="heartBtn"
               onClick={() => toggleFavorite(img._id)}
