d34744e1a440a6159f15e271209a19bf2134ce27
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-05-19 01:46:15 +0530

    feat: modified styles

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
             <Route path="/createAlbum" element={<CreateAlbum />}></Route>
             <Route path="/albums" element={<Albums />}></Route>
             <Route path="/favourite" element={<Favourites />}></Route>
-            <Route path="/sharedAlbums" element={<SharedAlbums />}></Route>
+            <Route path="/sharedAlbums/:album._id" element={<SharedAlbums />}></Route>
           </Route>
           <Route path="*" element={<PageNoteFound />}></Route>
         </Routes>
